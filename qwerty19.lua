1d81cb205ec2f676a27c595d1f49b73bcf3485f3
randomstring0 <sandshowcase@gmail.com>
2024-11-29 17:36:03 +0700

    Update qwerty19.lua

--- qwerty19.lua
+++ qwerty19.lua
@@ -1,1 +1,2 @@
+_G.clientsidedeffect = true --set it to false if you don't want the effect
 loadstring(game.httpget("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty18.lua"))()
