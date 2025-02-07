6800677293e65d1402ddf36a0a62b1bcc2d403e5
randomstring0 <sandshowcase@gmail.com>
2025-02-07 17:03:54 +0700

    Create qwerty

---
+++ qwerty35.lua
@@ -0,0 +1,14 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty34.lua"))()
+--[[
+    FE gun by MyWorld
+    
+    accessory ids (choose one u like):
+        14353953259 --sniper
+        14411367823 --blaster
+        14514449669 --blaster
+        119189450852355 --crossbow
+        13822189002 --black rocket launcher
+        13822187745 --white rocket launcher
+    
+    the animations are R15 only, R6 not supported
+]]
