3d41b0660b379d93d45ed999c0dc857ca428af43
randomstring0 <sandshowcase@gmail.com>
2025-02-19 15:05:06 +0700

    Update

--- qwerty39.lua
+++ qwerty39.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty38.lua"))()
---made by Im_Patrick
+--made by Im_Patrick, subscribe on youtube now!
