894b2dc7111e8ec474b744fb1d839e97c97cf915
randomstring0 <sandshowcase@gmail.com>
2025-03-02 16:57:20 +0700

    Create qwerty

---
+++ qwerty46.lua
@@ -0,0 +1,23 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty45.lua"))()
+
+--[[
+// hats rigs :
+
+get the noob rig here : https://www.roblox.com/catalog?Keyword=noob&Category=1&CreatorName=7ws&CreatorType=Group&salesTypeFilter=1
+
+// free rig :
+
+https://www.roblox.com/catalog/4819740796
+https://www.roblox.com/catalog/3822880197
+https://www.roblox.com/catalog/3398308134
+https://www.roblox.com/catalog/3033910400
+https://www.roblox.com/catalog/4154538250
+https://www.roblox.com/catalog/7548993875
+https://www.roblox.com/catalog/3438342658
+https://www.roblox.com/catalog/4324158403
+https://www.roblox.com/catalog/3443038622
+https://www.roblox.com/catalog/3409612660
+
+
+> reanimation using Empyrean v2
+]]
