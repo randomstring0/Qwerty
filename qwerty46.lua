f9042634b598c4a4473688c6cdf14efa30d96bc6
randomstring0 <sandshowcase@gmail.com>
2025-03-03 05:52:30 +0700

    Update qwerty

--- qwerty46.lua
+++ qwerty46.lua
@@ -12,7 +12,6 @@ https://www.roblox.com/catalog/3822880197
 https://www.roblox.com/catalog/3398308134
 https://www.roblox.com/catalog/3033910400
 https://www.roblox.com/catalog/4154538250
-https://www.roblox.com/catalog/7548993875
 https://www.roblox.com/catalog/3438342658
 https://www.roblox.com/catalog/4324158403
 https://www.roblox.com/catalog/3443038622
