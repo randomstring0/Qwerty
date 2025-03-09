1510fd0a6500a5269c9d1ba3d81848349db14347
randomstring0 <sandshowcase@gmail.com>
2025-03-09 14:38:25 +0700

    Create qwerty

---
+++ qwerty50.lua
@@ -0,0 +1,32 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty49.lua"))()
+
+--[[
+// hats rigs :
+
+
+// paid ink rig :
+
+https://www.roblox.com/catalog/85392395166623/Black-Left-Arm
+https://www.roblox.com/catalog/138364679836274/Torso-Extension
+https://www.roblox.com/catalog/131385506535381/Black-Right-Arm
+https://www.roblox.com/catalog/129462518582032/Black-Right-Leg
+https://www.roblox.com/catalog/131385506535381/Black-Right-Arm
+https://www.roblox.com/catalog/12740758463/Black-Faceless -- optional
+
+
+
+// free rig :
+
+https://www.roblox.com/catalog/4819740796
+https://www.roblox.com/catalog/3822880197
+https://www.roblox.com/catalog/3398308134
+https://www.roblox.com/catalog/3033910400
+https://www.roblox.com/catalog/4154538250
+https://www.roblox.com/catalog/3438342658
+https://www.roblox.com/catalog/4324158403
+https://www.roblox.com/catalog/3443038622
+https://www.roblox.com/catalog/3409612660
+
+
+> reanimation using fdless from patchma by myworld
+]]
