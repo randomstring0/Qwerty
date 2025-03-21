d074e18df0eadbfe5642a0634c6198320da9b8c0
randomstring0 <sandshowcase@gmail.com>
2025-03-21 14:00:27 +0700

    Update qwerty

--- qwerty46.lua
+++ qwerty46.lua
@@ -1,22 +1,23 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty45.lua"))()
 
 --[[
-// hats rigs :
+choice
 
-get the noob rig here : https://www.roblox.com/catalog?Keyword=noob&Category=1&CreatorName=7ws&CreatorType=Group&salesTypeFilter=1
+-- // white arm paid rigs for arm// --
 
-// free rig :
+https://www.roblox.com/catalog/90960046381276
+https://www.roblox.com/catalog/102599402682100
 
-https://www.roblox.com/catalog/4819740796
-https://www.roblox.com/catalog/3822880197
+-- // noob paid rigs for arm // --
+
+https://www.roblox.com/catalog/18641077392
+https://www.roblox.com/catalog/18641142410
+
+
+--// free hats rig for arm // --
+
 https://www.roblox.com/catalog/3398308134
-https://www.roblox.com/catalog/3033910400
-https://www.roblox.com/catalog/4154538250
-https://www.roblox.com/catalog/3438342658
-https://www.roblox.com/catalog/4324158403
 https://www.roblox.com/catalog/3443038622
-https://www.roblox.com/catalog/3409612660
 
-
-> reanimation using Empyrean v2
+> reanimation using Empyrean by emper
 ]]
