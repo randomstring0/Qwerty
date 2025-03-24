8d1c5b4265236361784ccf95622a8e20c43ae44d
randomstring0 <sandshowcase@gmail.com>
2025-03-24 14:53:39 +0700

    Create qwerty

---
+++ qwerty52.lua
@@ -0,0 +1,35 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty51.lua"))()
+
+
+
+--[[
+
+easy method to try and buy the hats : https://www.roblox.com/games/136733829568616/unknown-road-places
+
+
+
+///blade and ball///
+
+free : https://www.roblox.com/catalog/7548993875/Slasher (罗布乐思x凹凸世界 主题创作挑战赛活动 AOTU World Creation Challenge : it is a part of the AOTU World Creation Challenge event. It is a part of the Grey bundle. It can be obtained by collecting 40 Robots around the event game. ), https://www.roblox.com/catalog/7583370812/Nebula-Blade
+
+paid : 6685365462,15997771866,4504231783
+
+///body rigs///
+
+// another hats rig :
+
+get every rig here : https://www.roblox.com/games/136733829568616/unknown-road-places
+
+// free rig :
+
+https://www.roblox.com/catalog/4819740796
+https://www.roblox.com/catalog/3822880197
+https://www.roblox.com/catalog/3398308134
+https://www.roblox.com/catalog/3033910400
+https://www.roblox.com/catalog/4154538250
+https://www.roblox.com/catalog/3438342658
+https://www.roblox.com/catalog/4324158403
+https://www.roblox.com/catalog/3443038622
+https://www.roblox.com/catalog/3409612660
+
+]]
