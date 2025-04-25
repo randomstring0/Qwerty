9e94a7044117dc4454b6ad3bfc2b8466fe4ffdd8
randomstring0 <sandshowcase@gmail.com>
2025-04-25 18:01:08 +0700

    Create qwerty

---
+++ qwerty55.lua
@@ -0,0 +1,37 @@
+_G.Config = {
+    ["HatCollide"]     = false, -- Collision w/ hats
+    ["ReClaim"]        = true,  -- Re Claim falling hats
+    ["Fling"]          = true,  -- Activate flinging
+    ["HideCharacter"]  = true,  -- hide your character to void
+
+    ["FlingOption"] = {
+        ["HatFling"]        = false, -- Use hats to fling
+        ["Highlight"]       = true,  -- Glow targets
+        ["PredictionFling"] = false,  -- prediction fling
+        ["ToolFling"]       = true  -- use tool to fling
+    }
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty54.lua"))()
+
+
+--[[
+get the body rig on this game : https://www.roblox.com/games/136733829568616/unknown-road-places
+
+choose the sword :
+--paid :
+https://www.roblox.com/catalog/84513687194285/Rainbow-Neon-Sword-Back
+
+https://www.roblox.com/catalog/75133535004735/Yellow-Gold-Neon-Sword-Back
+
+https://www.roblox.com/catalog/124711435393884/Purple-Neon-Sword-Back
+
+-- free :
+https://www.roblox.com/catalog/7548993875/Slasher -- concanve event games
+
+how to wear multiple hats on roblox : https://youtu.be/G0ZeaGld6FY?si=HiK2Y6_0tku3AHiD
+on mobile you need extra web browser that can install extension like kiwi browser 
+
+> made by sedeecake
+> converted by rochips community
+]]
