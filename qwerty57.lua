8913b61522f583f90d79eecad992b68fad4bc27a
randomstring0 <sandshowcase@gmail.com>
2025-04-29 17:23:49 +0700

    Update qwerty

--- qwerty57.lua
+++ qwerty57.lua
@@ -1,7 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty56.lua"))()
 
 --[[
-This script will check if the executor you are using has a replicasignal
+This script will check if the executor you are using has a replicatesignal
 if the executor has a replicatesignal the script will have a permanentdeath function and support in many games such as brookhaven and almost any games
 and if it does not have a replicatesignal the script will automatically use the respawn loop method
 ]]
