0d695901ce75fc59b5c16f3bbd8ad261604e3bac
randomstring0 <sandshowcase@gmail.com>
2025-05-14 21:45:52 +0700

    Update qwerty

--- qwerty46.lua
+++ qwerty46.lua
@@ -1,8 +1,9 @@
 control = "mobile" -- pc / mobile
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty45.lua"))()
-
 --[[
+please read the texts below,
+I really feel annoyed with comments like "I always die and then repawn" and "can you make it use free hats" 
+even though everything has been explained in the explanation below this text
+
 
 testing game : https://www.roblox.com/games/136733829568616/unknown-road-places
 choice
@@ -28,5 +29,28 @@ if the executor has a replicatesignal the script will have a permanentdeath func
 and if it does not have a replicatesignal the script will automatically use the respawn loop method
 
 
+// can you make it without hats? for universal
+answer : no
+
+// does this include free hats?
+answer : yes 
+
+// does this script work for universal game?
+answer : yes
+
+// why my arm gone?
+answer : I'm guessing you're not wearing the hats I've linked above.
+
+// why the script not working / keep crashing ?
+answer : There are several possibilities why the script doesn't work, the first = the executor you are using is probably not supported, the second possibility = you copied the script incorrectly (you can copy all the text in this link https://github.com/randomstring0/Qwerty/edit/main/qwerty46.lua), and the third possibility = you spammed the script
+
+// how to wear 2 hats paid hats version on mobile?
+answer : The method is the same as above, only you need an additional application that can be used to download extensions, such as an application called "Kiwi Browser".
+
+
 > reanimation using Empyrean by emper
 ]]
+
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty45.lua"))()
+
