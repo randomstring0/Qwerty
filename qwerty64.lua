868ea94b183dc837b51d01e254bc6a51d4d6aa05
randomstring0 <sandshowcase@gmail.com>
2025-07-27 08:41:12 +0700

    Update qwerty

--- qwerty64.lua
+++ qwerty64.lua
@@ -1,2 +1,2 @@
- -- optional "-gh 75133535004735"
+ -- optional "-gh 75133535004735" org from serverside terraprisma by sedeecake converted to legacy fe script by ice
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty63.lua"))()
