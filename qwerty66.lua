11aa9498d2e4ae133ed065a0567be8d32e747da3
randomstring0 <sandshowcase@gmail.com>
2025-10-05 10:42:14 +0700

    Update qwerty

--- qwerty66.lua
+++ qwerty66.lua
@@ -1,2 +1,2 @@
-org from @Im_Patrick video Called Fe Noli
+--org from @Im_Patrick video Called Fe Noli
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty65.lua"))()
