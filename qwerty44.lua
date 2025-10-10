0ad74b9143babc7a3850e7188f80b989e5a5db57
randomstring0 <sandshowcase@gmail.com>
2025-10-10 17:16:13 +0700

    Update qwerty

--- qwerty44.lua
+++ qwerty44.lua
@@ -10,20 +10,7 @@ free : https://www.roblox.com/catalog/7548993875/Slasher (罗布乐思x凹凸世
 
 // hats rigs :
 
-get the noob rig here : https://www.roblox.com/catalog?Keyword=noob&Category=1&CreatorName=7ws&CreatorType=Group&salesTypeFilter=1
-
-// free rig :
-
-https://www.roblox.com/catalog/4819740796
-https://www.roblox.com/catalog/3822880197
-https://www.roblox.com/catalog/3398308134
-https://www.roblox.com/catalog/3033910400
-https://www.roblox.com/catalog/4154538250
-https://www.roblox.com/catalog/3438342658
-https://www.roblox.com/catalog/4324158403
-https://www.roblox.com/catalog/3443038622
-https://www.roblox.com/catalog/3409612660
-
+check out inside this game : https://www.roblox.com/games/136733829568616/unknown-road-places
 
 > reanimation using fdless from patchma by myworld
 ]]
