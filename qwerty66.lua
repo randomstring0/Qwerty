7267561996c996b4d05430eab675ba2fa5bca4b9
randomstring0 <sandshowcase@gmail.com>
2025-12-19 07:10:09 +0700

    Update qwerty

--- qwerty66.lua
+++ qwerty66.lua
@@ -1,2 +1,7 @@
 --org from @Im_Patrick video Called Fe Noli
+-- USE THIS IN THE Utility game called "just a baseplate." or "green baseplate." and the game creator is "dreammazestudio" or "the local maze" if you use this in another experience it will not work
+-- this script use to be just a baseplate script series
+
+-- universal version: https://www.youtube.com/watch?v=0x807I1DHFY&t=196s
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty65.lua"))()
