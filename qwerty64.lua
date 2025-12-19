34b4f7363a2d3e919544378ac4f1532f3d3f6117
randomstring0 <sandshowcase@gmail.com>
2025-12-19 07:12:16 +0700

    Update qwerty

--- qwerty64.lua
+++ qwerty64.lua
@@ -1,2 +1,6 @@
  -- optional "-gh 75133535004735" org from serverside terraprisma by sedeecake converted to legacy fe script by ice
+
+-- USE THIS IN THE Utility game called "just a baseplate." or "green baseplate." and the game creator is "dreammazestudio" or "the local maze" if you use this in another experience it will not work
+-- this script is use to be just a baseplate script series
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/randomstring0/Qwerty/refs/heads/main/qwerty63.lua"))()
